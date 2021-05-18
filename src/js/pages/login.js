1ff843f60e699dd9e40bf0ec5f4469cbd76cc54a
Lynn-Apedo <lynnapedo@gmail.com>
2021-05-18 17:33:10 +0200

    feat: form register and login

--- src/js/pages/login.js
+++ src/js/pages/login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Inputgroup from '../components/inputgroup';
+import Button from '../components/button';
 
 const Login = () => {
+    // const handleChange = async (event) => {
+        
+    // }
+    
+
   return (
     <div>
       <h1>C'est la page Login !</h1>
+      <form>
+        <Inputgroup type="email" name=" email"/>
+        <Inputgroup type="password" name="password"  minlength="8"/>
+         <Button name="M'inscrire"/>
+      </form>
     </div>
   );
 };
