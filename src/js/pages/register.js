09d78bd8614011b0ae3090defd3ead0d9374508c
Anne Morren <anne.morren@gmail.com>
2021-05-20 15:40:55 +0200

    register working

--- src/js/pages/register.js
+++ src/js/pages/register.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import Inputgroup from '../components/inputgroup';
-import Button from '../components/button';
+import React from "react";
+import Inputgroup from "../components/inputgroup";
+import Button from "../components/button";
+import Api from "../utils/api";
 
 const Register = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const body = {
+      lastname: "Jelly",
+      firstname: "Bean",
+      email: "bean@hopeitworks.com",
+      password: "sweet",
+    };
+
+    Api.post("http://localhost:3001/users", body);
+  };
+
   return (
     <div>
       <h1>Register page !</h1>
-      <form>
-        <Inputgroup type="text" name="firstname"/>
-        <Inputgroup type="text" name="lastname"/>
-        <Inputgroup type="email" name="email"/>
-        <Inputgroup type="password" name="password" minlength="8"/>
-        <Button name="M'inscrire"/>
+      <form onSubmit={handleSubmit}>
+        <Inputgroup type="text" name="firstname" />
+        <Inputgroup type="text" name="lastname" />
+        <Inputgroup type="email" name="email" />
+        <Inputgroup type="password" name="password" minlength="8" />
+        <Button name="M'inscrire" />
       </form>
-
     </div>
   );
 };
