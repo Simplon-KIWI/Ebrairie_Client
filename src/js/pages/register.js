d3e4ca7cd300749e539a7b2299989443174f86d0
Anne Morren <anne.morren@gmail.com>
2021-05-21 11:17:08 +0200

    register + protected routes functioning

--- src/js/pages/register.js
+++ src/js/pages/register.js
@@ -1,47 +1,47 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import api from '../../utils/api';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import api from "../../utils/api";
 
-import Inputgroup from '../components/inputgroup';
-import Button from '../components/button';
+import Inputgroup from "../components/inputgroup";
+import Button from "../components/button";
 
 const Register = () => {
   let history = useHistory();
 
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleChange = (inputValue, inputName) => {
-    if (inputName === 'firstname') setFirstname(inputValue);
-    if (inputName === 'lastname') setLastname(inputValue);
-    if (inputName === 'email') setEmail(inputValue);
-    if (inputName === 'password') setPassword(inputValue);
+    if (inputName === "firstname") setFirstname(inputValue);
+    if (inputName === "lastname") setLastname(inputValue);
+    if (inputName === "email") setEmail(inputValue);
+    if (inputName === "password") setPassword(inputValue);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const body = {
-      firstname: 'kiwi',
-      lastname: 'admin',
-      email: 'kiwi@admin.com',
-      password: 'iamadmin',
-      role: 'admin'
+      firstname,
+      lastname,
+      email,
+      password,
+      role: "admin",
     };
 
-    const result = await api.post('/admin/register', body);
+    const result = await api.post("/admin/register", body);
 
     try {
-      const result = await api.post('/admin/register', body);
-      console.log(result.status, 'STATUS');
-      if (result.status === 200) {
-        history.push('/login');
-        console.log('200 OK');
+      const result = await api.post("/admin/register", body);
+      console.log(result.status, "STATUS");
+      if (result.status === 201) {
+        history.push("/login");
+        console.log("200 OK");
       }
     } catch (error) {
-      console.log('connection failed');
+      console.log("connection failed");
       console.error(error.message);
     }
   };
@@ -49,10 +49,15 @@ const Register = () => {
     <div>
       <h1>Register page !</h1>
       <form onSubmit={handleSubmit}>
-        <Inputgroup type="text" name="firstname" />
-        <Inputgroup type="text" name="lastname" />
-        <Inputgroup type="email" name="email" />
-        <Inputgroup type="password" name="password" minlength="8" />
+        <Inputgroup type="text" name="firstname" value={handleChange} />
+        <Inputgroup type="text" name="lastname" value={handleChange} />
+        <Inputgroup type="email" name="email" value={handleChange} />
+        <Inputgroup
+          type="password"
+          name="password"
+          minlength="8"
+          value={handleChange}
+        />
         <Button name="M'inscrire" />
       </form>
     </div>
